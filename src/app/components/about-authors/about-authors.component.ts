ce3566fd21af2a06f81de8d61f916b5ad30995c2
marc gil bawa-an <20234231@s.ubaguio.edu>
2025-04-22 15:06:34 +0800

    created components

---
+++ src/app/components/about-authors/about-authors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-authors',
+  imports: [],
+  templateUrl: './about-authors.component.html',
+  styleUrl: './about-authors.component.css'
+})
+export class AboutAuthorsComponent {
+
+}
---
+++ src/app/components/about-topic/about-topic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-topic',
+  imports: [],
+  templateUrl: './about-topic.component.html',
+  styleUrl: './about-topic.component.css'
+})
+export class AboutTopicComponent {
+
+}
