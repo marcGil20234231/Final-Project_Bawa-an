94c49ccaf3569fdd2bb6291702ccc670d4082c60
marc gil bawa-an <20234231@s.ubaguio.edu>
2025-04-22 15:55:12 +0800

    added description of about-authors

--- src/app/components/about-authors/about-authors.component.ts
+++ src/app/components/about-authors/about-authors.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+
+interface Author {
+  name: string;
+  role: string;
+  imageUrl: string;
+  bio: string;
+  socialLinks: {
+    linkedin?: string;
+    github?: string;
+    email: string;
+  };
+}
 
 @Component({
   selector: 'app-about-authors',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './about-authors.component.html',
   styleUrl: './about-authors.component.css'
 })
 export class AboutAuthorsComponent {
-
+  authors: Author[] = [
+    {
+      name: 'Marc Gil Bawa-an',
+      role: 'Lead Developer',
+      imageUrl: '',
+      bio: 'BSIT student',
+      socialLinks: {
+        linkedin: '',
+        github: '',
+        email: '20234231@s.ubaguio.edu'
+      }
+    }
+  ];
 }
